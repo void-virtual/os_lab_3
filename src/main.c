3655eddb235f3e4cf1f66b72de4e081b2ace37d3
ilya <ilya.semenov89099@yandex.ru>
2019-11-02 10:43:49 +0300

    final

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include "adjacency_list.h"
 #include "queue.h"
+#include <ctype.h>
 // лаба 3, вариант 21
 // поиск кратчайшего пути в графе поиском в ширину
 // граф задается матрицей смежности
@@ -26,31 +27,31 @@ int min(int lhs, int rhs) {
 //result - там хранятся подсчитанные расстояния до вершин, или -1 если не подсчитано
 //max_thread_count - эта переменная уменьшается при создании нового потока,
 //таким образом будет создано столько потоков сколько нужно
-//mutexes - мьютексы для синхронизации доступа к result
+//mutex - мьютекс для синхронизации доступа к result
 //list - список смежности для графа, будем считать, что граф связный
 void *bfs(void *args) {
     thread_params* params = (thread_params*) args;
     vector recount;
     v_init(&recount);
-    pthread_mutex_lock(params->mutex);
+
     for (int i = 0; i < params->list->vecs[params->current_vertex].size; ++i) {
-        
         int n_v = params->list->vecs[params->current_vertex].data[i].vertex_number;
         int n_vl = params->list->vecs[params->current_vertex].data[i].length;
+        pthread_mutex_lock(params->mutex);
         if (params->result[n_v] == -1 || params->result[n_v] > params->result[params->current_vertex] + n_vl) {
             params->result[n_v] = params->result[params->current_vertex] + n_vl;
             v_push(&recount, (pair){.vertex_number = n_v, .length = n_vl});
         }
-        
+        pthread_mutex_unlock(params->mutex);
     }
-    pthread_mutex_unlock(params->mutex);
+
 
     pthread_mutex_lock(params->thread_count_mutex);
     int can_create_threads = min(*(params->max_thread_count), recount.size);
     *(params->max_thread_count) -= can_create_threads;
     pthread_mutex_unlock(params->thread_count_mutex);
 
-    pthread_t threads[can_create_threads];
+    pthread_t* threads = malloc(sizeof(pthread_t) * can_create_threads);
     thread_params* new_params = malloc(sizeof(thread_params) * can_create_threads);
     for (int i = 0; i < can_create_threads; ++i) {
         new_params[i] = *params;
@@ -63,10 +64,10 @@ void *bfs(void *args) {
     for (int i = can_create_threads; i < recount.size; ++i) {
         q_push(&q, recount.data[i].vertex_number);
     }
-    pthread_mutex_lock(params->mutex);
+
     while (!q_empty(&q)) {
         int cur_vertex = q_pop(&q);
-        
+        pthread_mutex_lock(params->mutex);
         for (int i = 0; i < params->list->vecs[cur_vertex].size; ++i) {
             int neighbour_vertex = params->list->vecs[cur_vertex].data[i].vertex_number;
             int neighbour_distance = params->list->vecs[cur_vertex].data[i].length;
@@ -76,9 +77,9 @@ void *bfs(void *args) {
                 q_push(&q, neighbour_vertex);
             }
         }
-        
+        pthread_mutex_unlock(params->mutex);
     }
-    pthread_mutex_unlock(params->mutex);
+
 
     for (int i = 0; i < can_create_threads; ++i) {
         pthread_join(threads[i], NULL);
@@ -88,21 +89,44 @@ void *bfs(void *args) {
     pthread_mutex_unlock(params->thread_count_mutex);
     v_destroy(&recount);
     free(new_params);
+    free(threads);
     pthread_exit(NULL);
 }
 
-int main() {
-    int n;
-    printf("Enter size of matrix\n");
-    scanf("%d", &n);
-    int matrix[n][n];
-    printf("Enter matrix\n");
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            scanf("%d", &matrix[i][j]);
+int parse_count(const char* str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (!isdigit(str[i])) {
+            return -1;
         }
     }
+    return atoi(str);
+}
 
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Not enough arguments\n");
+        exit(1);
+    }
+    FILE* file = fopen(argv[1], "r");
+    if (!file) {
+        printf("Wrong filename\n");
+        exit(1);
+    }
+    int *max_thread_count = malloc(sizeof(int));
+    *max_thread_count = parse_count(argv[2]);
+    if (*max_thread_count <= 0) {
+        printf("Incorrect thread count\n");
+        exit(1);
+    }
+    int n;
+    fscanf(file, "%d", &n);
+    int matrix[n][n];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            fscanf(file, "%d", &matrix[i][j]);
+        }
+    }
+    fclose(file);
     adjacency_list *list = malloc(sizeof(adjacency_list));
     a_init(list);
     a_resize(list, n);
@@ -113,8 +137,9 @@ int main() {
             }
         }
     }
+
     size_t vertex_number;
-    int *max_thread_count = malloc(sizeof(int));
+
     int *result = malloc(sizeof(int) * n);
     for (int i = 0; i < n; ++i) {
         result[i] = -1;
@@ -128,16 +153,9 @@ int main() {
     printf("Enter vertex to start search\n");
     scanf("%ld", &vertex_number);
     while (vertex_number > n || vertex_number == 0) {
-        printf("Vertex number must be < %d\n and > 0", n);
+        printf("Vertex number must be < %d\n and > 0\n", n);
         printf("Enter vertex to start search\n");
         scanf("%ld", &vertex_number);
-    }
-    printf("Enter max thread count\n");
-    scanf("%d", max_thread_count);
-    while (*max_thread_count < 1) {
-        printf("Max thread count must be not less than 1");
-        printf("Enter max thread count\n");
-        scanf("%d", max_thread_count);
     }
 
     vertex_number--;
@@ -151,18 +169,13 @@ int main() {
             .mutex = mutex,
             .thread_count_mutex = thread_count_mutex};
 
-    pthread_t first_thread;
-    pthread_create(&first_thread, NULL, bfs, &parameters);
-    pthread_join(first_thread, NULL);
-
+    pthread_t* first_thread = malloc(sizeof(pthread_t));
+    pthread_create(first_thread, NULL, bfs, &parameters);
+    pthread_join(*first_thread, NULL);
     for (int i = 0; i < n; ++i) {
         printf("%d:%d ", i + 1, result[i]);
     }
+    printf("\n");
     return 0;
 }
 
-//0 3 5 0 0
-//3 0 0 6 1
-//5 0 0 4 0
-//0 6 4 0 1
-//0 1 0 1 0
